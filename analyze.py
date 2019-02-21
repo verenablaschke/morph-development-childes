6529f987055b1c1b06591afdb977d759043aeb09
Verena Blaschke <verena.blaschke@gmx.de>
2019-02-21 12:39:04 +0100

    Get no. of specific word/morpheme occurrences per age

--- analyze.py
+++ analyze.py
@@ -1,39 +1,79 @@
-import nltk
+import glob
+from enum import Enum
 from nltk.corpus.reader import CHILDESCorpusReader
+
 
 DATA_PATH = 'data'
 
 
-def count_occurrences(corpus, query_word, verbose=True):
+class Condition(Enum):
+    MATCH = 0
+    SUFFIX = 1
+
+
+def match(query_word, query_condition, word):
+    if query_condition == Condition.MATCH:
+        return word == query_word
+    elif query_condition == Condition.SUFFIX:
+        return word.endswith('-' + query_word)
+    else:
+        raise ValueError('Condition {} does not exist.'
+                         .format(query_condition))
+
+
+def count_occurrences(corpus, query_word, query_condition, results, verbose=True):
+    age = corpus.age(month=True)[0]
     if verbose:
-        print(corpus.fileids(), corpus.age(month=True))
+        print(corpus.fileids(), age)
     n_sents = 0
     n_occ = 0
     for sent in corpus.tagged_sents(speaker='CHI',
                                     strip_space=True,
-                                    stem=True   # Inflection details.
+                                    stem=True  # Inflection details.
                                     ):
         n_sents += 1
         for (word, _) in sent:
-            if word == query_word:
+            if match(query_word, query_condition, word):
                 n_occ += 1
+
+    if n_sents > 0:
+        try:
+            prev_counts = results[(query_word, query_condition)][age]
+            results[(query_word, query_condition)][age] = [sum(x) for x in zip(prev_counts, (n_occ, n_sents))]
+        except KeyError:
+            try:
+                results[(query_word, query_condition)][age] = [n_occ, n_sents]
+            except KeyError:
+                results[(query_word, query_condition)] = {age: [n_occ, n_sents]}
+
     if verbose:
         print(n_occ, n_sents, n_occ / n_sents if n_sents > 0 else 0)
         print()
-    return n_occ, n_sents
+    return results
 
 
-instances = []
-for year in range(6):
-    for month in range(1, 13):
-        age = year * 12 + month
-        n_occ, n_sents = count_occurrences(
-            corpus=CHILDESCorpusReader(DATA_PATH,
-                                       '.*/.*/{:02d}{:02d}.*.xml'
-                                       .format(year, month)),
-            query_word='on')
-        if n_sents > 0:
-            instances.append((age, n_occ, n_sents))
+# TODO get the number of sentences only once
+# results:
+# {(query_word, query condition) -> {age -> [n_occ, n_sent]}}
+results = {}
+i = 0  # TODO del
+for f in glob.glob('data/Brown/Adam/*.xml'):
+    f = f.replace('\\', '/')[5:]
+    results = count_occurrences(CHILDESCorpusReader(DATA_PATH, f),
+                                'PRESP',
+                                Condition.SUFFIX,
+                                results)
+    results = count_occurrences(CHILDESCorpusReader(DATA_PATH, f),
+                                'on',
+                                Condition.MATCH,
+                                results)
+    results = count_occurrences(CHILDESCorpusReader(DATA_PATH, f),
+                                'in',
+                                Condition.MATCH,
+                                results)
+    i += 1  # TODO del
+    if i > 3:
+        break
 
-for i in instances:
-    print(i)
+for r in results:
+    print(r, results[r])
