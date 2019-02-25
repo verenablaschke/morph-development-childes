ef975463c46a53d8d900f178d49285f04bf89c44
Verena Blaschke <verena.blaschke@gmx.de>
2019-02-25 10:49:39 +0100

    Separate visualization script; add comparison to adult utterances

--- analyze.py
+++ analyze.py
@@ -1,13 +1,13 @@
-import copy
 import glob
-import numpy as np
-import matplotlib.pyplot as plt
+import sys
 # Imports from other files in this directory:
 from corpusreader import CHILDESMorphFileReader
 from match import Matcher
+from visualize import visualize
 
 
 DATA_PATH = 'data'
+AGE_ERROR_MSG = 'Age is None. Skipping file'
 
 
 def count_occurrences(corpus, matcher, results, verbose=True):
@@ -16,7 +16,8 @@ def count_occurrences(corpus, matcher, results, verbose=True):
         print(matcher)
         print(corpus.fileids(), age)
     if age is None:
-        print('Age is None. Skipping file')
+        print(AGE_ERROR_MSG)
+        sys.stderr.write('{}(s) {}.\n'.format(AGE_ERROR_MSG, corpus.fileids()))
         return results
     n_sents = 0
     n_occ = 0
@@ -27,28 +28,38 @@ def count_occurrences(corpus, matcher, results, verbose=True):
         for entry in sent:
             if matcher.match(entry):
                 n_occ += 1
-                # if verbose:
-                #     print(sent)
+                if verbose:
+                    print(sent)
+    n_adult = 0
+    for sent in corpus.tagged_morph_sents(speaker=['MOT', 'FAT', 'INV'],
+                                          strip_space=True,
+                                          ):
+        for entry in sent:
+            if matcher.match(entry):
+                n_adult += 1
+                if verbose:
+                    print('ADULT:', sent)
     if n_sents > 0:  # TODO smooth?
         matcher_str = matcher.__str__()
         try:
             prev_counts = results[matcher_str][age]
             results[matcher_str][age] = \
-                [sum(x) for x in zip(prev_counts, (n_occ, n_sents))]
+                [sum(x) for x in zip(prev_counts, (n_occ, n_adult, n_sents))]
         except KeyError:
             try:
-                results[matcher_str][age] = [n_occ, n_sents]
+                results[matcher_str][age] = [n_occ, n_adult, n_sents]
             except KeyError:
                 results[matcher_str] = \
-                    {age: [n_occ, n_sents]}
+                    {age: [n_occ, n_adult, n_sents]}
 
     if verbose:
-        print(n_occ, n_sents, n_occ / n_sents if n_sents > 0 else 0)
+        print(n_occ, n_adult, n_sents,
+              n_occ / n_sents if n_sents > 0 else -1,
+              n_occ / n_adult if n_adult > 0 else -1)
         print()
     return results
 
 
-# TODO traverse each file only once, i.e. one corpus reader per file
 matchers = [Matcher('infl', infl='PRESP'),                       # 1. -ing
             Matcher('form', form='in'),                          # 2./3. in
             Matcher('form', form='on'),                          # 2./3. on
@@ -66,8 +77,6 @@ matchers = [Matcher('infl', infl='PRESP'),                       # 1. -ing
 # results:
 # {query -> {age -> [n_occ, n_sent]}}
 results = {}
-# i = 0  # TODO del
-# for f in glob.glob('data/Brown/Adam/*.xml'):
 for f in glob.glob('data/*/*/*.xml'):
     f = f.replace('\\', '/')[5:]
     for matcher in matchers:
@@ -75,48 +84,5 @@ for f in glob.glob('data/*/*/*.xml'):
                                     matcher,
                                     results)
 
-    # i += 1  # TODO del
-    # if i > 2:
-        # break
-
-# Visualization of the results
-fig, ax = plt.subplots()
-plots = []
-cmap = plt.get_cmap('jet')
-n_plots = len(results)
-colours = cmap(np.linspace(0, 1.0, n_plots))
-i = 1
-
-min_month, max_month = -1, -1
-
-for (query, r), col in zip(results.items(), colours):
-    print(query, r)
-    months = sorted(r.keys())
-
-    # Get the dimensions of the graph.
-    if min_month == -1 or min_month > months[0]:
-        min_month = months[0]
-    if max_month < months[-1]:
-        max_month = months[-1]
-
-    plot, = plt.plot(months, [r[month][0] / r[month][1] for month in months],
-                     color=col, label=query)
-    plots.append(plot)
-    i += 1
-
-plt.grid()
-xticks = np.arange(min_month, max_month + 1, step=3).tolist()
-xtick_labels = copy.deepcopy(xticks)
-for y in range(2, 6):
-    plt.axvline(x=y * 12, color='r')
-    xticks.append(y * 12)
-    xtick_labels[-1] = '{} yrs'.format(y)
-
-ax.set_xticks(xticks)
-ax.set_xticklabels(xtick_labels)
-ax.set(xlabel='Age in months',
-       ylabel='Occurrences per utterance',
-       title='')
-plt.legend(handles=plots, loc=2)
-plt.show()
-# plt.savefig('filename.png', bbox_inches='tight')
+visualize(results, compare_total=True)
+visualize(results, compare_total=False)
---
+++ visualize.py
@@ -0,0 +1,72 @@
+import copy
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+EMPTY_MONTH_MSG = 'No {}entries for month {}, query={}.'
+
+
+def visualize(results, compare_total, display=True, filename=None):
+    fig, ax = plt.subplots()
+    plots = []
+    cmap = plt.get_cmap('jet')
+    n_plots = len(results)
+    colours = cmap(np.linspace(0, 1.0, n_plots))
+    i = 1
+
+    if compare_total:
+        comp_idx = 1
+    else:
+        comp_idx = 2
+
+    min_month, max_month = -1, -1
+
+    for (query, r), col in zip(results.items(), colours):
+        print(query, r)
+        months = sorted(r.keys())
+
+        # Get the dimensions of the graph.
+        if min_month == -1 or min_month > months[0]:
+            min_month = months[0]
+        if max_month < months[-1]:
+            max_month = months[-1]
+
+        months_ok = []
+        entries_ok = []
+        for month in months:
+            if r[month][comp_idx] is None or r[month][comp_idx] <= 0:
+                msg = EMPTY_MONTH_MSG.format('' if compare_total else 'adult ',
+                                             month, query)
+                print(msg)
+                sys.stderr.write(msg + '\n')
+                continue
+            months_ok.append(month)
+            entries_ok.append(r[month][0] / r[month][comp_idx])
+
+        plot, = plt.plot(months_ok, entries_ok, color=col, label=query)
+        plots.append(plot)
+        i += 1
+
+    plt.grid()
+    xticks = np.arange(min_month, max_month + 1, step=3).tolist()
+    xtick_labels = copy.deepcopy(xticks)
+    for y in range(2, 6):
+        plt.axvline(x=y * 12, color='r')
+        xticks.append(y * 12)
+        xtick_labels[-1] = '{} yrs'.format(y)
+
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xtick_labels)
+    if compare_total:
+        ylabel = 'Occurrences per utterance'
+    else:
+        ylabel = 'Occurrences per adult utterance'
+    ax.set(xlabel='Age in months',
+           ylabel=ylabel,
+           title='')
+    plt.legend(handles=plots, loc=2)
+    if display:
+        plt.show()
+    if filename is not None:
+        plt.savefig('{}.png'.format(filename), bbox_inches='tight')
